75fe5c6894cf85a6a04ce82f5d1f62281b99f988
Adrian Preuß <info@adi-code.de>
2017-03-20 20:02:47 +0100

    Updating config.php

--- core/config.php
+++ core/config.php
@@ -1,8 +1,10 @@
 <?php
 	define('DATABASE_HOSTNAME',		'localhost');
-	define('DATABASE_PORT',				3306);
-	define('DATABASE_NAME',				'ctn');
+	define('DATABASE_PORT',			3306);
+	define('DATABASE_NAME',			'ctn');
 	define('DATABASE_USERNAME',		'root');
 	define('DATABASE_PASSWORD',		'');
-	define('DATABASE_PREFIX',				'ctn_');
+	define('DATABASE_PREFIX',		'ctn_');
+	
+	define('CREDITS_START',			2500);
 ?>
