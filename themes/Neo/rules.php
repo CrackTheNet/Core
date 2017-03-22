cf3b38f06293074a6ad24787dcef0f0076ade241
Adrian Preuß <info@adi-code.de>
2017-03-22 08:55:06 +0100

    fix UTF-8 encoding

--- themes/Neo/rules.php
+++ themes/Neo/rules.php
@@ -1,40 +1,33 @@
 <?php
 	$template->getHeader();
 ?>
-	<div class="container">
-		<div class="row">
-			<div class="col-sm-6">
-				<h1><?php print $title; ?></h1>
-				<p><?php print $description; ?></p>
-				<?php
-					for($index = 0; $index < $count / 2; $index++) {
-						$rule = $rules[$index];
-						?>
-							<h2 id="rules_<?php print ($index + 1); ?>"><strong>§<?php print ($index + 1); ?></strong> <?php print $rule->title; ?></h2>
-							<p><?php print $rule->text; ?></p>
-						<?php
-					}
-				?>
-			</div>
-			<div class="col-sm-6">				
-				<br />
-				<br />
-				<br />
-				<?php
-					for(; $index < $count; $index++) {
-						$rule = $rules[$index];
-						?>
-							<h2 id="rules_<?php print ($index + 1); ?>"><strong>§<?php print ($index + 1); ?></strong> <?php print $rule->title; ?></h2>
-							<p><?php print $rule->text; ?></p>
-						<?php
-					}
-				?>
-			</div>
-		</div>
-	</div>
-	<ctn-map>
-		<canvas id="particles"></canvas>
-	</ctn-map>
+<div class="col-sm-6">
+	<h1><?php print $title; ?></h1>
+	<p><?php print $description; ?></p>
+	<?php
+		for($index = 0; $index < $count / 2; $index++) {
+			$rule = $rules[$index];
+			?>
+				<h2 id="rules_<?php print ($index + 1); ?>"><strong>§<?php print ($index + 1); ?></strong> <?php print $rule->title; ?></h2>
+				<p><?php print $rule->text; ?></p>
+			<?php
+		}
+	?>
+</div>
+<div class="col-sm-6">				
+	<br />
+	<br />
+	<br />
+	<?php
+		for(; $index < $count; $index++) {
+			$rule = $rules[$index];
+			?>
+				<h2 id="rules_<?php print ($index + 1); ?>"><strong>§<?php print ($index + 1); ?></strong> <?php print $rule->title; ?></h2>
+				<p><?php print $rule->text; ?></p>
+			<?php
+		}
+	?>
+</div>
 <?php
 	$template->getFooter();
 ?>
