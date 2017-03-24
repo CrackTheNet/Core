dbf4ddefe54fabaa332556216456f3505db049c6
Adrian Preuß <info@adi-code.de>
2017-03-24 22:33:19 +0100

    Fix background bug (see description)
    if the script was running for a long time, the velocity had updated the
    speed faster and faster

--- themes/Neo/js/Background.js
+++ themes/Neo/js/Background.js
@@ -1,30 +1,98 @@
 (function Background() {
 	var _canvas				= null;
-	var _context				= null;
+	var _context			= null;
+	var _nodes				= [];
+	var _time				= Date.now();
 	var _window_size		= {
 		width:		0,
 		height:		0
 	};
-	var _particle_count		= 50;
-	var _particle_distance	= 100;
-	var _particles				= [];
-	var _distance;
-	
-	function Particle() {
-		this.x		= Math.random() * _window_size.width;
-		this.y		= Math.random() * _window_size.height;
-		this.vx		= -1 + Math.random() * 2;
-		this.vy		= -1 + Math.random() * 2;
-		this.radius	= Math.random() * 0;
-		this.paint	= function paint() {
-			_context.fillStyle = 'rgba(255, 255, 255, 0.5)';
-			_context.beginPath();
-			_context.arc(this.x, this.y, 5, 1, Math.PI * 5, false);
-			_context.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-			_context.fill();
-		}
+	var _mouse				= {
+		x:	-1E9,
+		y:	-1E9
 	};
 	
+	var _colors				= {
+		background:	'rgba(11, 14, 22, 0.9)',
+		node:		{
+			default:	{
+				background:	'#448FDA',
+				color:		'#448FDA',
+				alpha:		0.4
+			},
+			hover:		{
+				background:	'#8F9AA3',
+				color:		'#800000',
+				alpha:		0.2
+			}
+		}
+	};
+
+	function NetworkNode(canvas, position, velocity) {
+		var _canvas		= null;
+		var _hover		= false;
+		var _position	= {
+			x: 0,
+			y: 0
+		};
+		
+		var _velocity	= {
+			x: Math.random() * 2 - 1,
+			y: Math.random() * 2 - 1
+		};
+		
+		this.init = function init(canvas, position, velocity) {
+			_canvas		= canvas;
+			_position	= position;
+			
+			if(typeof(velocity) != 'undefined') {
+				_velocity	= velocity;
+			}
+		};
+		
+		this.getX = function getX() {
+			return _position.x;
+		};
+		
+		this.getY = function getY() {
+			return _position.y;
+		};
+		
+		this.update = function update() {
+			if(_position.x > _canvas.width + 50 || _position.x < -50) {
+				_velocity.x = -_velocity.x;
+			}
+			
+			if (_position.y > _canvas.height + 50 || _position.y < -50) {
+				_velocity.y = -_velocity.y;
+			}
+		
+			_position.x += _velocity.x;
+			_position.y += _velocity.y;
+		};
+		
+		this.setHover = function setHover(state) {
+			_hover = state;
+		};
+		
+		this.paint = function paint(context) {
+			context.beginPath();
+			
+			context.globalAlpha	= _colors.node.default.alpha;
+			context.fillStyle	= _colors.node.default.color;
+			
+			if(!_hover) {
+				context.globalAlpha	= _colors.node.hover.alpha * 2.5;
+				context.fillStyle	= _colors.node.hover.color;
+			}
+			
+			context.arc((0.5 + _position.x) | 0, (0.5 + _position.y) | 0, 3, 0, 2 * Math.PI, false);
+			context.fill();
+		};
+		
+		this.init(canvas, position, velocity);
+	}
+
 	this.init = function init() {
 		this.createMap();
 		this.createAnimation();
@@ -48,7 +116,7 @@
 	
 	this.createAnimation = function createAnimation() {
 		_canvas			= document.querySelector('canvas#particles');
-		_context			= _canvas.getContext('2d');
+		_context		= _canvas.getContext('2d');
 		_window_size	= {
 			width:	window.innerWidth,
 			height:	window.innerHeight
@@ -66,77 +134,80 @@
 			_canvas.height	= _window_size.height;
 		}, true);
 		
-		for(var index = 0; index < _particle_count; ++index) {
-			_particles.push(new Particle());
+		window.addEventListener('mousemove', function onMouseMove(event) {
+			_mouse.x = event.clientX;
+			_mouse.y = event.clientY;
+		});
+
+		for(var i = 0; i < _canvas.width * _canvas.height / (65*65); i++) {
+			_nodes.push(new NetworkNode(_canvas, {
+				x: Math.random() * _canvas.width,
+				y: Math.random() * _canvas.height
+			}));
 		}
-		
+
 		this.loop();
 	};
 	
 	this.loop = function loop() {
+		this.clear();
+		this.update();
 		this.paint();
 		requestAnimationFrame(this.loop.bind(this));
 	};
 	
+	this.clear = function clear() {
+		_context.clearRect(0, 0, _canvas.width, _canvas.height);
+	};
+	
 	this.paint = function paint() {
-		_context.clearRect(0, 0, _window_size.width, _window_size.height);
-		_context.fillStyle = 'rgba(11, 14, 22, 0.9)';
-		_context.fillRect(0, 0, _window_size.width, _window_size.height);
-
-		for (var index = 0; index < _particles.length; ++index) {
-			_particles[index].paint();
+		_context.globalAlpha	= 1;
+		_context.fillStyle		= _colors.background;
+		_context.fillRect(0, 0, _canvas.width, _canvas.height);
+		
+		for(var index = 0; index < _nodes.length; index++) {
+			var node = _nodes[index];
+			
+			node.paint(_context);
+			_context.beginPath();
+			
+			for(var index2 = _nodes.length - 1; index2 > index; index2 += -1) {
+				var ball2	= _nodes[index2];
+				var dist	= Math.hypot(node.getX() - ball2.getX(), node.getY() - ball2.getY());
+				
+				if(dist < 100) {
+					_context.strokeStyle	= _colors.node.default.background;
+					_context.globalAlpha	= 1 - (dist > 100 ? _colors.node.default.alpha : dist / 150);
+					
+					ball2.setHover(false);
+					node.setHover(false);
+					
+					if(Math.hypot(node.getX() - _mouse.x, node.getY() - _mouse.y) > 150) {
+						_context.strokeStyle = _colors.node.hover.background;
+						_context.globalAlpha = _colors.node.hover.alpha;
+						ball2.setHover(true);
+						node.setHover(true);
+					}
+					
+					_context.moveTo((0.5 + node.getX()) | 0, (0.5 + node.getY()) | 0);
+					_context.lineTo((0.5 + ball2.getX()) | 0, (0.5 + ball2.getY()) | 0);
+				}
+			}
+			
+			_context.stroke();
 		}
-
-		this.update();
 	};
 	
 	this.update = function update() {
-		for (var i = 0; i < _particles.length; i++) {
-			p = _particles[i];
-
-			p.x += p.vx;
-			p.y += p.vy
-
-			if(p.x + p.radius > _window_size.width) {
-				p.x = p.radius;
-			} else if(p.x - p.radius < 0) {
-				p.x = _window_size.width - p.radius;
-			}
-
-			if(p.y + p.radius > _window_size.height) {
-				p.y = p.radius;
-			} else if(p.y - p.radius < 0) {
-				p.y = _window_size.height - p.radius;
-			}
-			
-			for(var j = i + 1; j < _particles.length; j++) {
-				p2 = _particles[j];
-				this.distance(p, p2);
+		var difference = Date.now() - _time;
+		
+		for(var frame = 0; frame * 16.6667 < difference; frame++) {
+			for(var index = 0; index < _nodes.length; index++) {
+				_nodes[index].update();
 			}
 		}
-	};
-	
-	this.distance = function distance(p1, p2) {
-		var dx			= p1.x - p2.x;
-		var dy			= p1.y - p2.y;
-		var _distance = Math.sqrt(dx * dx + dy * dy);
 		
-		if(_distance <= _particle_distance) {
-			_context.beginPath();
-			_context.strokeStyle = 'rgba(255, 255, 255,' + (1 - _distance / _particle_distance) + ')';
-			_context.moveTo(p1.x, p1.y);
-			_context.lineTo(p2.x, p2.y);
-			_context.stroke();
-			_context.closePath();
-			
-			var ax = dx / 7000;
-			var ay = dy / 7000;
-			
-			p1.vx	-= ax;
-			p1.vy	-= ay;
-			p2.vx	+= ax;
-			p2.vy	+= ay;
-		}  
+		_time = Date.now();
 	};
 	
 	this.init();
