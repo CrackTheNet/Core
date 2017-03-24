6cf222a5d3edca0693b04ad3e927bf3ef315017e
Adrian Preuß <info@adi-code.de>
2017-03-25 00:07:08 +0100

    adding bootstrap scripts

--- themes/Neo/footer.php
+++ themes/Neo/footer.php
@@ -9,6 +9,8 @@
 			$template->includeJS([
 				'https://cdn.polyfill.io/v2/polyfill.min.js?features=requestAnimationFrame,Element.prototype.classList,URL',
 				'https://openlayers.org/en/v4.0.1/build/ol.js',
+				'https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js',
+				'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js',
 				'js/Header.js',
 				'js/Background.js'
 			], true);
