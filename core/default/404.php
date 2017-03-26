c25c91f9a2d52333572fa67429337c04df8827ba
Adrian Preuß <info@adi-code.de>
2017-03-26 11:39:30 +0200

    adding default 404 page

---
+++ core/default/404.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="de">
+	<head>
+		<title>CrackTheNet - 404 Not Found</title>
+		<link rel="shortcut icon" type="image/png" href="favicon.ico" />
+		<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,700" />
+		<meta charset="UTF-8" />
+		<style type="text/css">
+			::selection, ::-webkit-selection, *::selection {
+				background: transparent;
+				background: rgba(255, 255, 255, 0);
+			}
+
+			* {
+				cursor: default;
+				outline: none !important;
+				-webkit-overflow-scrolling:	touch;
+				-webkit-font-smoothing: antialiased;
+				user-select: none;
+			}
+
+			html, body {
+				font-family: Roboto, Arial;
+				width: 100%;
+				height: 100%;
+				padding: 0;
+				margin: 0;
+				text-align: center;
+				overflow: none;
+				-webkit-box-sizing: border-box;
+				box-sizing: border-box;
+				font-smoothing: antialiased;
+				font-smooth: always;
+				text-rendering: optimizeLegibility;
+			}
+			
+			html {
+				display: table;
+			}
+			
+			body {
+				display: table-cell;
+				vertical-align: middle;
+				background: #444444;
+				color: #FFFFFF;
+			}
+			
+			h1 {
+				display: block;
+				width: 277px;
+				height: 72px;
+				margin: 0 auto;
+				background: url('https://www.crack-the-net.com/themes/Neo/assets/logo.png');
+			}
+			
+			h1 span {
+				display: none;
+			}
+			
+			a {
+				color: #FFFFFF;
+				text-decoration: none;
+				display: inline-block;
+				background: rgba(0, 0, 0, 0.3);
+				border-radius: 3px;
+				padding: 8px 15px;
+				border: none;
+				font-size: 20px;
+				margin: 5px 0;
+				cursor: pointer;
+			}
+
+			a * {
+				cursor: pointer;
+			}
+
+			a:hover {
+				background: rgba(0, 0, 0, 0.2);
+			}
+		</style>
+	</head>
+	<body>
+		<h1><span>CrackTheNet</span></h1>
+		<p>Hier gibt es leider nichts zu sehen.</p>
+		<a href="https://www.crack-the.net.com/">Zur Hauptseite</a>
+	</body>
+</html>
