9c160262914cc1af3a1e98f2080d1f7c9a0f768c
Adrian Preuß <info@adi-code.de>
2017-03-26 11:38:45 +0200

    Adding version

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
-	define('PATH',	dirname(__FILE__));
+	define('VERSION',	'1.0.0');
+	define('PATH',		dirname(__FILE__));
 	define('DS',		DIRECTORY_SEPARATOR);
 	
 	require_once(sprintf('%s/core/classes/core.class.php', PATH));
