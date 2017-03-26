3dcb18b8afd9db98b7fca6a2740e48ed53553cdf
Adrian Preuß <info@adi-code.de>
2017-03-26 11:40:30 +0200

    adding alt attribute on logo (validity), comment out currently unused sites

--- themes/Neo/header.php
+++ themes/Neo/header.php
@@ -2,17 +2,49 @@
 	use \CTN\Auth;
 ?>
 <!DOCTYPE html>
-<html lang="<?php print $template->getLanguage(true); ?>">
+<html lang="<?php print $template->getLanguage(true); ?>" xml:lang="de" xmlns="http://www.w3.org/1999/xhtml">
 	<head>
 		<title><?php print $template->getTitle(); ?></title>
 		<meta charset="<?php print $template->getCharset(); ?>" />
+		<meta name="description" content="CrackTheNet ist ein browserbasiertes Onlinespiel. Ihr bekommt einen Computer in einem virtuellen (nicht existenten) Netzwerk zugewiesen." />
+		<meta name="keywords" content="CrackTheNet, HackTheNet, HTN, CTN, Browsergame, Browserspiel" />
+		<meta name="author" content="Adrian Preuß" />
+		<meta property="site_name" content="CrackTheNet" />
+		
+		<!-- Open Graph -->
+        <meta property="og:title" content="<?php print $template->getTitle(); ?>" />
+        <meta property="og:site_name" content="CrackTheNet" />
+        <meta property="og:type" content="game" />
+        <meta property="og:description" content="CrackTheNet ist ein browserbasiertes Onlinespiel. Ihr bekommt einen Computer in einem virtuellen (nicht existenten) Netzwerk zugewiesen. Entwickelt euren Prozessor und andere Items weiter und verdient Geld. Bald seid Ihr soweit dass Ihr Waffen Viren Trojaner etc entwickeln und gegen eure Feinde einsetzen könnt." />
+        <meta property="og:image" content="<?php print $template->getThemeURL('assets/icon.png'); ?>g"/>
+        <meta property="og:url" content="<?php print $template->getURL(''); ?>"/>
+        
+        <!-- Dublin-Core  -->
+		<meta name="DC.Creator" content="Adrian Preuß">
+		<meta name="DC.Rights" content="OpenSource">
+		<meta name="DC.Publisher" content="CrackTheNet">
+		<meta name="DC.Date" content="2017-03-25">
+		<meta name="DC.Language" content="<?php print $template->getLanguage(true); ?>">
+		<meta name="DC.Type" content="Collection">
+		<meta name="DC.Format" content="text/html">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		
+		<script type="application/ld+json">
+			{
+				"@context" : "http://schema.org",
+				"@type" : "WebSite",
+				"name" : "CrackTheNet",
+				"alternateName" : "HackTheNet",
+				"url" : "<?php print $template->getURL(''); ?>"
+			}
+		</script>
+
 		<link rel="icon" type="image/png" href="<?php print $template->getThemeURL('assets/icon.png'); ?>" />
 		<?php
 			$template->includeCSS([
-				'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700',
-				'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css',
-				'https://openlayers.org/en/v4.0.1/css/ol.css',
+				'css/fonts.css',
+				'css/bootstrap.min.css',
+				'css/ol.css',
 				'css/style.css'
 			]);
 			
@@ -29,7 +61,7 @@
 							<span class="icon ti-layout-grid3-alt"></span>
 						</button>
 						<a class="navbar-brand" href="<?php print $template->getURL('/'); ?>">
-							<img src="<?php print $template->getThemeURL('assets/logo.png'); ?>" alt="" />
+							<img src="<?php print $template->getThemeURL('assets/logo.png'); ?>" alt="CrackTheNet" />
 						</a>
 					</div>
 					<div class="collapse navbar-collapse" id="navbar-collapse">
@@ -62,14 +94,14 @@
 								?>
 									<ul class="nav nav-pills nav-stacked">
 										<li class="active"><a href="<?php print $template->getURL('/overview'); ?>"><i class="icon-chevron-right"></i> Übersicht</a></li>
-										<li><a href=""><i class="icon-chevron-right"></i> Nachrichten (0)</a></li>
+										<li><a href="<?php print $template->getURL('/messages'); ?>"><i class="icon-chevron-right"></i> Nachrichten (<?php print $messages->countUnread(); ?>)</a></li>
 										<li><a href="<?php print $template->getURL('/computers'); ?>"><i class="icon-chevron-right"></i> Computer</a></li>
-										<li><a href="#"><i class="icon-chevron-right"></i> AngriffsCenter</a></li>
+										<!--<li><a href="#"><i class="icon-chevron-right"></i> AngriffsCenter</a></li>
 										<li><a href="#"><i class="icon-chevron-right"></i> Cluster</a></li>
 										<li><a href="#"><i class="icon-chevron-right"></i> Netzwerkumgebung</a></li>
 										<li><a href="#"><i class="icon-chevron-right"></i> Tools</a></li>
 										<li><a href="#"><i class="icon-chevron-right"></i> Rangliste</a></li>
-										<li><a href="#"><i class="icon-chevron-right"></i> Lotterie</a></li>
+										<li><a href="#"><i class="icon-chevron-right"></i> Lotterie</a></li>-->
 										<li><a href="<?php print $template->getURL('/logout'); ?>"><i class="icon-chevron-right"></i> Abmelden</a></li>
 									</ul>
 								<?php
