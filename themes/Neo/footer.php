6f8769029e061fe31334ad6650d185c47f152630
Adrian Preuß <info@adi-code.de>
2017-03-26 11:39:55 +0200

    move Theme scripts

--- themes/Neo/footer.php
+++ themes/Neo/footer.php
@@ -7,10 +7,10 @@
 		</ctn-map>
 		<?php			
 			$template->includeJS([
-				'https://cdn.polyfill.io/v2/polyfill.min.js?features=requestAnimationFrame,Element.prototype.classList,URL',
-				'https://openlayers.org/en/v4.0.1/build/ol.js',
-				'https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js',
-				'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js',
+				'js/Polyfill.min.js',
+				'js/ol.min.js',
+				'js/jQuery.min.js',
+				'js/bootstrap.min.js',
 				'js/Header.js',
 				'js/Background.js'
 			], true);
